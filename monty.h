a4ec8157f618361730fa0c71280db4b0639aaaf8
Abdulhamid <hamidejisanusi@gmail.com>
2023-08-30 22:14:33 +0000

    Updating header files

--- monty.h
+++ monty.h
@@ -12,9 +12,9 @@ extern stack_t *stack;
 
 /* PROTOTYPES */
 void execute_input(const char *input, unsigned int line_number);
-void push(stack_t **stack, int value);
-void pall(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
+void push(__attribute((unused))stack_t **stack, int value);
+void pall(__attribute((unused))stack_t **stack, __attribute((unused))unsigned int line_number);
+void pint(__attribute((unused))stack_t **stack, unsigned int line_number);
 
 
 #endif
